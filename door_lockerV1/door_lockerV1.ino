631848c1737e876bf343bb6fc39495d1aeff5a87
choeminjun <choeminjun@naver.com>
2019-07-16 15:57:01 +0900

    backup

---
+++ door_lockerV1/door_lockerV1.ino
@@ -0,0 +1,145 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
+boolean roomopen = false;
+boolean lcd_backlight = true;
+
+int lockbutton = 5;
+
+int last_on = millis();
+
+byte semark[] = {
+  B00100,
+  B01000,
+  B10110,
+  B10110,
+  B10110,
+  B10000,
+  B01000,
+  B00100
+};
+
+byte lock[] = {
+  B01110,
+  B10001,
+  B10001,
+  B11111,
+  B10101,
+  B10101,
+  B10001,
+  B11111
+};
+
+byte _open[] = {
+  B01110,
+  B10001,
+  B00001,
+  B11111,
+  B10101,
+  B10101,
+  B10001,
+  B11111
+};
+
+void setup()
+{
+
+  pinMode(lockbutton, INPUT);
+
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  lcd.init();
+  lcd.createChar(0, semark);
+  lcd.createChar(1, lock);
+  lcd.createChar(2, _open);
+  lcd.backlight(); // backlight를 On 시킵니다.
+  lcd.setCursor(0, 0);
+  lcd.write(0);
+  lcd.print("<CMJ's ROOM>");
+  lcd.setCursor(0, 1);
+  lcd.write(1);
+  lcd.print(">>ROOM LOCKED<<");
+  lcd.setCursor(0, 2);
+  lcd.print("Card ID: ");
+
+}
+void loop()
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial())
+  {
+    backlight_on();
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content = "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "EA 59 61 02") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    roomopen = true;
+    lcd.setCursor(0, 1);
+    lcd.write(2);
+    lcd.print(">>ROOM UNLOCKED<<");
+    while (true) {
+      if (digitalRead(lockbutton) == HIGH) {
+        roomopen = false;
+        backlight_on();
+        lcd.setCursor(0, 1);
+        lcd.write(1);
+        lcd.print(">>ROOM LOCKED<<");
+        break;
+      }
+      if (((millis() - last_on) > 15000) && (lcd_backlight)) {
+        backlight_off();
+      }
+    }
+  }
+
+  else   {
+    Serial.println(" Access denied");
+    delay(100);
+  }
+  if (((millis() - last_on) > 15000) && (lcd_backlight)) {
+    backlight_off();
+  }
+  Serial.println((millis() - last_on));
+  Serial.println(lcd_backlight);
+
+}
+
+void backlight_on() {
+  lcd.backlight();
+  last_on = millis();
+  lcd_backlight = true;
+}
+
+void backlight_off() {
+  lcd.noBacklight();
+  lcd_backlight = false;
+}
