0c97ab32e563bfe5b020c8fd41ea7162ae6a46dc
choeminjun <choeminjun@naver.com>
2019-09-15 11:15:15 +0900

    fanrotater.ino multithread

--- fanrotater/fanrotater.ino
+++ fanrotater/fanrotater.ino
@@ -1,18 +1,14 @@
 #include <Servo.h>
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
+#include <TimedAction.h>
 
 static int buttonPin = 2;
 static int timebuttonpin = 3;
 int mode = 0;
 int timemode = 0;
-int sleeptime = 1000;
+int sleeptime = 1;
 int servodegrees = 10;
-
-int step1 = 0;
-int step2 = 0;
-int step3 = 0;
-int step4 = 0;
 
 unsigned long lastmillis = millis();
 boolean servo_direction = true;
@@ -21,25 +17,8 @@ boolean servoon = true;
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 Servo servo;
 
-void setup()
+void checkdegrees()
 {
-    lcd.init();      // lcd를 사용을 시작합니다.
-    lcd.backlight(); // backlight를 On 시킵니다.
-    lcd.setCursor(0,0);
-    lcd.print("FanControl");
-    
-    
-
-    pinMode(buttonPin, INPUT);
-    pinMode(timebuttonpin, INPUT);
-    servo.attach(12);
-    servo.write(90);
-    Serial.begin(9600);
-}
-
-void loop()
-{
-    lcd_print(sleeptime, servodegrees, servoon);
     if (digitalRead(buttonPin) == HIGH)
     {
         mode++;
@@ -50,25 +29,25 @@ void loop()
         switch (mode)
         {
         case 0:
-            servodegrees = 12;
+            servodegrees = 6;
             break;
         case 1:
-            servodegrees = 48;
+            servodegrees = 24;
             break;
         case 2:
-            servodegrees = 100;
+            servodegrees = 50;
             break;
         case 3:
-            servodegrees = 160;
+            servodegrees = 80;
             break;
         }
-        int divison_ = servodegrees / 4;
-        step1 = division;
-        setp2
         lcd_print(sleeptime, servodegrees, servoon);
         delay(100);
     }
+}
 
+void checksleep()
+{
     if (digitalRead(timebuttonpin) == HIGH)
     {
         timemode++;
@@ -80,16 +59,16 @@ void loop()
         switch (timemode)
         {
         case 0:
-            sleeptime = 500;
+            sleeptime = 1;
             break;
         case 1:
-            sleeptime = 1000;
+            sleeptime = 5;
             break;
         case 2:
-            sleeptime = 1200;
+            sleeptime = 15;
             break;
         case 3:
-            sleeptime = 1500;
+            sleeptime = 30;
             break;
         case 4:
             servoon = false;
@@ -98,6 +77,80 @@ void loop()
         lcd_print(sleeptime, servodegrees, servoon);
         delay(100);
     }
+}
+
+void setup()
+{
+    lcd.init();      // lcd를 사용을 시작합니다.
+    lcd.backlight(); // backlight를 On 시킵니다.
+    lcd.setCursor(0, 0);
+    lcd.print("FanControl");
+
+    pinMode(buttonPin, INPUT);
+    pinMode(timebuttonpin, INPUT);
+    servo.attach(12);
+    servo.write(90);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    lcd_print(sleeptime, servodegrees, servoon);
+    // if (digitalRead(buttonPin) == HIGH)
+    // {
+    //     mode++;
+    //     if (mode >= 4)
+    //     {
+    //         mode = 0;
+    //     }
+    //     switch (mode)
+    //     {
+    //     case 0:
+    //         servodegrees = 6;
+    //         break;
+    //     case 1:
+    //         servodegrees = 24;
+    //         break;
+    //     case 2:
+    //         servodegrees = 50;
+    //         break;
+    //     case 3:
+    //         servodegrees = 80;
+    //         break;
+    //     }
+    //     lcd_print(sleeptime, servodegrees, servoon);
+    //     delay(100);
+    // }
+
+    // if (digitalRead(timebuttonpin) == HIGH)
+    // {
+    //     timemode++;
+    //     if (timemode >= 5)
+    //     {
+    //         timemode = 0;
+    //     }
+    //     servoon = true;
+    //     switch (timemode)
+    //     {
+    //     case 0:
+    //         sleeptime = 1;
+    //         break;
+    //     case 1:
+    //         sleeptime = 5;
+    //         break;
+    //     case 2:
+    //         sleeptime = 15;
+    //         break;
+    //     case 3:
+    //         sleeptime = 30;
+    //         break;
+    //     case 4:
+    //         servoon = false;
+    //         break;
+    //     }
+    //     lcd_print(sleeptime, servodegrees, servoon);
+    //     delay(100);
+    // }
 
     // if ((millis() - lastmillis) >= sleeptime)
     // {
@@ -107,24 +160,61 @@ void loop()
     //     lastmillis = millis();
     //     delay(sleeptime);
     // }
-    if (servoon && (millis() - lastmillis) > sleeptime)
+    // if (servoon && (millis() - lastmillis) > sleeptime)
+    // {
+    //     if (servo_direction)
+    //     {
+    //         // servo.write(servodegrees);
+
+    //         for (int i; servodegrees > i; i++)
+    //         {
+    //             servo.write(servo.read() + 2);
+    //         }
+
+    //         servo_direction = !servo_direction;
+    //         lastmillis = millis();
+    //     }
+    //     else
+    //     {
+    //         for (int i; servodegrees > i; i++)
+    //         {
+    //             servo.write(servo.read() - 2);
+    //         }
+    //         servo_direction = !servo_direction;
+    //         lastmillis = millis();
+    //     }
+    // }
+    // else
+    // {
+    //     servo.write(servo.read());
+    // }
+
+    if (servo_direction)
     {
-        if (servo_direction)
+        // servo.write(servodegrees);
+
+        for (int i = 0; servodegrees >= i; i++)
         {
-            servo.write(servodegrees);
-            servo_direction = !servo_direction;
-            lastmillis = millis();
+            servo.write(servo.read() + 2);
+            delay(sleeptime);
+            Serial.println("Loop1");
+            Serial.println(i);
         }
-        else
-        {
-            servo.write(90);
-            servo_direction = !servo_direction;
-            lastmillis = millis();
-        }
+
+        servo_direction = !servo_direction;
+        // lastmillis = millis();
     }
     else
     {
-        servo.write(servo.read());
+        for (int i = 0; servodegrees >= i; i++)
+        {
+            servo.write(servo.read() - 2);
+            delay(sleeptime);
+            Serial.println("Loop1");
+            Serial.println(i);
+        }
+        servo_direction = !servo_direction;
+        // lastmillis = millis();
     }
 
     Serial.print("sleeptime:::");
@@ -138,15 +228,15 @@ void loop()
 
 void lcd_print(int sleeptime, int servodegrees, bool servoon)
 {
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print("Sleeptime:");
     lcd.print(sleeptime);
 
-    lcd.setCursor(0,2);
+    lcd.setCursor(0, 2);
     lcd.print("ServoD:");
     lcd.print(servodegrees);
 
-    lcd.setCursor(0,3);
+    lcd.setCursor(0, 3);
     lcd.print(" ServoOn:");
     lcd.print(servoon);
 }
