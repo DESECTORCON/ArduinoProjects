8603303c430bad4779fb70c1a95af51037cb2ed4
choeminjun <choeminjun@naver.com>
2019-12-25 15:02:31 +0900

    backup

--- RCCAR/rccar.ino
+++ RCCAR/rccar.ino
@@ -4,6 +4,10 @@ SoftwareSerial BT(10, 11);
 Servo leftservo;
 Servo rightservo;
 
+int light1 = 5;
+int light2 = 6;
+int light3 = 7;
+
 int left = 2;
 int right = 3;
 int leftspeed = 90;
@@ -13,6 +17,10 @@ void setup()
 {
 
   pinMode(13, OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+
   BT.begin(9600);
   BT.println("AR:connected");
   leftservo.attach(left);
@@ -26,26 +34,33 @@ void loop()
       a=(BT.read());
 
       if (a == 'a'){
-        leftspeed = 130;
-        rightspeed = 180;
+        leftspeed = 140;
+        rightspeed = 0;
         BT.println("AR:ac");
       }
       if (a == 'b'){
-        leftspeed = 40;
-        rightspeed = 90;BT.println("AR:ac");
+        leftspeed = 50;
+        rightspeed = 180;BT.println("AR:ac");
           
       }
       if (a == 'c'){
         leftspeed = 130;
-        rightspeed = 90;BT.println("AR:ac");
+        rightspeed = 180;BT.println("AR:ac");
       }
       if (a == 'd'){
-        leftspeed = 90;
-        rightspeed = 180;BT.println("AR:ac");
+        leftspeed = 50;
+        rightspeed = 0;BT.println("AR:ac");
       }
       if (a == 'e'){
         leftspeed = 90;
         rightspeed = 90;BT.println("AR:ac");
+      }
+
+      if (a == 'x'){
+        digitalWrite(7,HIGH);digitalWrite(6,HIGH);digitalWrite(5,HIGH);
+      }
+      if (a == 'z'){
+        digitalWrite(7,LOW);digitalWrite(6,LOW);digitalWrite(5,LOW);
       }
 
   }
